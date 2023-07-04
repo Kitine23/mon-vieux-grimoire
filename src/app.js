23aefd1d97025749e710ea87b963abff5e227353
Kitine23 <duong.christine23@gmail.com>
2023-07-04 11:58:03 +0200

    continue mongodb functions in routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import { connectDB } from './db.js'
 import { BookModel } from './models/Book.js'
 import uploads from './middlewares/uploads.js'
+import { UserModel } from './models/User.js'
 
 dotenv.config()
 
@@ -19,17 +20,45 @@ app.use(
 )
 app.use(cors())
 
-app.post('/api/auth/signup', (req, res) => {
-  res.json('succès')
+app.post('/api/auth/signup', async (req, res) => {
+  const user = req.body
+  try {
+    const UserObject = new UserModel(user)
+    await UserObject.save()
+  } catch (error) {
+    console.error(error)
+    res.status(422).json({ message: 'validation error' })
+  }
+
+  res.status(201).json({ message: 'created' })
 })
 
-app.post('/api/auth/login', (req, res) => {
-  res.json('succès!')
+app.post('/api/auth/login', async (req, res) => {
+  let user
+  const { email, password } = req.body
+  try {
+    user = await UserModel.findOne({ email, password }).exec()
+    if (!user) return res.status(401).json({ message: 'wrong credentials' })
+  } catch (error) {
+    console.error(error)
+    res.status(422).json({ message: 'validation error' })
+  }
+
+  res.json({ userId: user._id, token: 'dev' })
 })
 
 app.get('/api/books', async (req, res) => {
   const books = await BookModel.find()
   res.json(books)
+})
+
+app.get('/api/books/bestrating', async (req, res) => {
+  const threeBestAverageNoteBooks = await BookModel.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .exec()
+
+  res.json(threeBestAverageNoteBooks)
 })
 
 app.get('/api/books/:id', async (req, res) => {
@@ -44,10 +73,6 @@ app.get('/api/books/:id', async (req, res) => {
   }
 
   res.json(book)
-})
-
-app.get('/api/books/bestrating', async (req, res) => {
-  res.json('succès!')
 })
 
 app.post('/api/books', uploads, async (req, res) => {
@@ -76,12 +101,28 @@ app.put('/api/books/:id', (req, res) => {
   res.json('succès!')
 })
 
-app.delete('/api/books/:id', (req, res) => {
-  res.json('succès!')
+app.delete('/api/books/:id', async (req, res) => {
+  try {
+    await BookModel.deleteOne({ _id: req.params.id })
+  } catch (error) {
+    console.error(error)
+    res.status(404).json({ message: 'not found' })
+    return
+  }
+  res.json({ message: 'deleted' })
 })
 
-app.post('/api/books/:id/rating', (req, res) => {
-  res.json('succès!')
+app.post('/api/books/:id/rating', async (req, res) => {
+  try {
+    const book = await BookModel.findById(req.params.id)
+    book.ratings.push(req.body)
+    const BookObject = new BookModel(book)
+    await BookObject.save()
+  } catch (error) {
+    console.error(error)
+    res.status(422).json({ message: 'validation error' })
+  }
+  res.json(book)
 })
 
 export { app }
